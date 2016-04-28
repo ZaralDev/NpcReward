96cd44d3bcb190ade363c23ffc2df0a9294f20d0
BuildTools <samuelmarchese7@gmail.com>
2016-04-28 20:33:39 +0200

    Fixed bug

--- src/main/java/fr/zaral/npcreward/listeners/PlayerListener.java
+++ src/main/java/fr/zaral/npcreward/listeners/PlayerListener.java
@@ -5,6 +5,7 @@ import fr.zaral.npcreward.objects.Stage;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 
@@ -24,13 +25,16 @@ public class PlayerListener implements Listener {
         Player player = event.getPlayer();
         Stage stage = pl.getStageManager().isInStage(player);
         ItemStack clicked = event.getItem();
-        if (stage != null) {
-            event.setCancelled(true);
-            return;
-        } else if (clicked != null && clicked.hasItemMeta()) {
-            if (clicked.equals(pl.getSettings().getItemReward())) {
+        Action action = event.getAction();
+        if (action == Action.RIGHT_CLICK_AIR || action == Action.RIGHT_CLICK_BLOCK) {
+            if (stage != null) {
                 event.setCancelled(true);
-                pl.getStageManager().newStage(player, null, true);
+                return;
+            } else if (clicked != null && clicked.hasItemMeta()) {
+                if (clicked.equals(pl.getSettings().getItemReward())) {
+                    event.setCancelled(true);
+                    pl.getStageManager().newStage(player, null, true);
+                }
             }
         }
     }
